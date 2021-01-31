d3a2b030f7b41317da93d250191ea28fafbbc317
furkan <ozdemir1furkan@gmail.com>
2021-01-31 08:32:44 -0300

    project is already in remote repo , commiting more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,10 @@
 public class Hello {
+    // more stuff
+    // Mac user and windows users are happy to see commit dialog finally
+
+    // I have already published on Github.com
+    // now I am ready to make more commits
+    // but do I need to share again ????
+    // Absolutely no
+    // IT'S TIME COMMIT AND JUST PUSH
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        // EU4 is good at IntelliJ
+    // write your code here
+        System.out.println("Hello");///
+
 
 
 
